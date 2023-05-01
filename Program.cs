b8dbe74d618ce92287f2f24cdd154d47cd0bb8e8
Denis <oumniy@gmail.com>
2023-05-01 20:18:58 +0300

    zada4a so vtorim 4islom

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите трехзначное число:");
+int number = int.Parse(Console.ReadLine());
+int number2 = number/10;
+System.Console.WriteLine($"Вторая цифра числа {number} - {number2%10}");
